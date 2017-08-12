c2ee38ad21379de546843ec84d942a91ab420b6c
SriDakshinaMurthy <divinebalaji@yahoo.com>
2017-08-11 22:44:18 -0600

    part 02 04 02

--- onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/kzn/onlineshopping/controller/PageController.java
@@ -1,6 +1,7 @@
 package net.kzn.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -15,7 +16,28 @@ public class PageController {
 		mv.addObject("greeting", "Welcome to Spring web MVC");
 		
 		return mv;
-		
-	}
+			}
+	
+/*	@RequestMapping(value = "/test")
+	public ModelAndView test(@RequestParam(value = "greeting", required = false)String greeting)
+	{
+		if (greeting == null) {
+			greeting = "Hello There";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+		}*/
+	
+	@RequestMapping(value = "/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting)
+	{
+		if (greeting == null) {
+			greeting = "Hello There";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+		}
 	
 }
